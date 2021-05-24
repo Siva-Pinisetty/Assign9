b8e93cbe3b14c3b16a2a5f50d67a599d5a9ef60d
balu-pinisetty <balupinisetty@gmail.com>
2021-05-24 08:35:29 +0530

    Wage for a month

--- empwage.sh
+++ empwage.sh
@@ -4,21 +4,24 @@ echo "Welcome to Employee Wage Computation"
 empPerHr=20
 empFulHr=8
 empPrtHr=4
-random=$((RANDOM%3))
-
-case $random in
-
-	2)	echo "Employee is Present"
-	Salary=$(($empPerHr*$empFulHr))
-	echo "and his salary is $Salary"
-		;;
-
-	1)	echo "Employee is Part time Present"
-	Salary=$(($empPerHr*$empPrtHr))
-	echo "and his salary is $Salary"
-		;;
-	*)	echo "Employee is Absent"
-	Salary=0
-	echo " and his salary is $Salary"
-		;;
-esac
+month=20
+empwage=0
+for ((i=1; i<=$month; i++))
+do
+	random=$((RANDOM%3))
+	case $random in
+		2)	echo "Employee is Present"
+		Salary=$(($empPerHr*$empFulHr))
+		empwage=$(($empwage+$Salary))
+			;;
+		1)	echo "Employee is Part time Present"
+		Salary=$(($empPerHr*$empPrtHr))
+		empwage=$(($empwage+$Salary))
+			;;
+		*)	echo "Employee is Absent"
+		Salary=0
+		empwage=$(($empwage+$Salary))
+			;;
+	esac
+done
+echo "Salary for a month $empwage"
