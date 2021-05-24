01d443f050525c9628e2495bc75590c9a3800a31
balu-pinisetty <balupinisetty@gmail.com>
2021-05-24 07:54:53 +0530

    Calculating Daily Employee Wage

--- empwage.sh
+++ empwage.sh
@@ -2,10 +2,16 @@
 echo "Welcome to Employee Wage Computation"
 
 isPresent=1
+empPerHr=20
+empFulHr=8
 random=$((RANDOM%2))
 if [ $isPresent -eq $random ]
 then
 	echo "Employee is Present"
+	Salary=$(($empPerHr*$empFulHr))
+	echo "and his salary is $Salary"
 else
 	echo "Employee is Absent"
+	Salary=0
+	echo " and his salary is $Salary"
 fi
