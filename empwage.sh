3d7eae82b09dc82bb213e5d97883c08dd2d7fe90
balu-pinisetty <balupinisetty@gmail.com>
2021-05-24 08:12:33 +0530

    Converting to Switchcase method

--- empwage.sh
+++ empwage.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
 
-FullPrst=2
-PrtTmPrst=1
 empPerHr=20
 empFulHr=8
 empPrtHr=4
 random=$((RANDOM%3))
-if [ $FullPrst -eq $random ]
-then
-	echo "Employee is Present"
+
+case $random in
+
+	2)	echo "Employee is Present"
 	Salary=$(($empPerHr*$empFulHr))
 	echo "and his salary is $Salary"
-elif [ $PrtTmPrst -eq $random ]
-then
-	echo "Employee is Part time Present"
+		;;
+
+	1)	echo "Employee is Part time Present"
 	Salary=$(($empPerHr*$empPrtHr))
 	echo "and his salary is $Salary"
-else
-	echo "Employee is Absent"
+		;;
+	*)	echo "Employee is Absent"
 	Salary=0
 	echo " and his salary is $Salary"
-fi
+		;;
+esac
