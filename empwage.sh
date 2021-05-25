9c97774650dc3ca9dabad20433d64511169f547c
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 00:51:34 +0530

    Storing Daily Wages

--- empwage.sh
+++ empwage.sh
@@ -7,35 +7,16 @@ case $1 in
 	0) echo "0";;
 esac
 }
-
+declare -a Day
 echo "Welcome to Employee Wage Computation"
-wrkhrs=0
 empPerHr=20
-empwage=0
+Totalwage=0
 for ((i=1; i<=20; i++))
 do
-	if [ $wrkhrs -lt 100 ]
-	then
-		ran=$((RANDOM%3))
-		case $ran in
-		2)
-		#echo "Employee is Present"
-		Salary=$(($empPerHr*`wrkhrs $ran`))
-		empwage=$(($empwage+$Salary))
-		wrkhrs=$(($wrkhrs+`wrkhrs $ran`))
-			;;
-		1)	empPrtHr=4
-		#echo "Employee is Part time Present"
-		Salary=$(($empPerHr*`wrkhrs $ran`))
-		empwage=$(($empwage+$Salary))
-		wrkhrs=$(($wrkhrs+`wrkhrs $ran`))
-			;;
-		*)	Salary=0
-		#echo "Employee is Absent"
-			;;
-		esac
-	else
-		break
-	fi
+	ran=$((RANDOM%3))
+	Day[$i]=$(($empPerHr*`wrkhrs $ran`))
+	Totalwage=$(($Totalwage+${Day[$i]}))
 done
-echo "Salary of $empwage credited in $(($i-1)) th day for $wrkhrs hours"
+
+echo "${Day[@]} are the daily wages"
+echo "Totalwage is $Totalwage"
